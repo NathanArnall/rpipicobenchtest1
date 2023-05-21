fde21526d5c8d641407e738b92a4cab078731b42
NathanArnall <90006919+NathanArnall@users.noreply.github.com>
2023-05-20 19:35:35 -0500

    Add files via upload

---
+++ bench.py
@@ -0,0 +1,34 @@
+#Raspberry Pi Pico Benchmark
+from machine import freq
+from time import sleep, ticks_cpu, ticks_ms
+loops = 1500 #Iterations to run through
+clockspeedmhz = 50 #Clockspeed of CPU in MHz. Recommended range is 20-270
+machine.freq(clockspeedmhz*1000000) #Set CPU speed in Hz
+initcputicks = ticks_cpu() #Grabs current number of ticks the CPU has been on
+print("Initial ticks: " + str(ticks_cpu()))
+currentticks = ticks_cpu() - initcputicks
+loopct = 0
+initms = ticks_ms()
+def bench():
+    #Start benchmarking code
+    print ("Your code should go here") #Or leave this as the benchmark
+    #End benchmarking code
+    
+while loopct < loops + 1:
+    currentticks = ticks_cpu() - initcputicks
+    loopct += 1
+    bench()
+    #Printing 1 line costs a lot of ticks (30 each non-empty print + ~2 ticks per character)
+    #print("Current ticks: " + str(currentticks))
+    #print("Current loops: " + str(tickct))
+    #print("Average ticks per benchmark): " + str((currentticks)/tickct))
+
+#Clear some space and dump those stats
+print ()
+print ()
+print ()
+print ()
+print ()
+print(str(loops) + " loops took " + str(currentticks) + " ticks to complete and " + str((ticks_ms() - initms)/1000) + " seconds")
+print("You averaged " + str((currentticks)/loops) + " ticks per loop")
+print("CPU Stats: " + str(machine.freq()/1000000) + " MHz")
